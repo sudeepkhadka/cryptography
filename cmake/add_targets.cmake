576cfe8f21460bcb91ebe589f10f6671e4553ad5
schhetri <sudeep.khadkachhetri.mil@army.mil>
2024-09-09 13:04:45 -0400

    Completed practice for cmake builds for SSLE

--- cmake/add_targets.cmake
+++ cmake/add_targets.cmake
@@ -1,16 +1,17 @@
 function(add_all_targets CMAKE_CHECKS)
-    set(OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/bin)
+    include(${CMAKE_SOURCE_DIR}/cmake/config.cmake)
     set(COMPILE_OPT -Wall -O3 -Wextra -pedantic --std=c17)
     
     if (EXISTS ${CMAKE_SOURCE_DIR}/src/deploy_rc4.c)
         add_executable(deploy_rc4 ${CMAKE_SOURCE_DIR}/src/deploy_rc4.c)
     endif()
     
-    if (EXISTS ${CMAKE_SOURCE_DIR}/src/simple_rc4.c)
-        add_executable(simple_rc4 ${CMAKE_SOURCE_DIR}/src/simple_rc4.c)
-    endif()
+    # if (EXISTS ${CMAKE_SOURCE_DIR}/src/simple_rc4.c)
+    #     add_executable(simple_rc4 ${CMAKE_SOURCE_DIR}/src/simple_rc4.c)
+    # endif()
 
-    set(TARGETS_LIST deploy_rc4 simple_rc4)
+    # set(TARGETS_LIST deploy_rc4 simple_rc4)
+    set(TARGETS_LIST deploy_rc4 )
 
     # Strip all the release executable to reduce its size
     foreach(target ${TARGETS_LIST})
@@ -21,7 +22,7 @@ function(add_all_targets CMAKE_CHECKS)
             COMMENT "Stripping ${target} executable"
         )
         set_target_properties(${target} PROPERTIES
-        RUNTIME_OUTPUT_DIRECTORY ${OUTPUT_DIRECTORY})
+                RUNTIME_OUTPUT_DIRECTORY ${OUTPUT_DIRECTORY})
         set_target_properties(${target} PROPERTIES COMPILE_OPTIONS ${COMPILE_OPT})
     endforeach()
 
--- cmake/add_tests.cmake
+++ cmake/add_tests.cmake
@@ -1,9 +1,15 @@
 function(add_all_tests)
-    set(OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/bin)
+    include(${CMAKE_SOURCE_DIR}/cmake/config.cmake)
     
     if (EXISTS "${CMAKE_SOURCE_DIR}/src/deploy_rc4.c" AND EXISTS "${CMAKE_SOURCE_DIR}/tests/test_rc4.c")
         add_library(rc4_lib SHARED ${CMAKE_SOURCE_DIR}/src/deploy_rc4.c)
         add_executable(test_rc4 ${CMAKE_SOURCE_DIR}/tests/test_rc4.c)
         target_link_libraries(test_rc4 rc4_lib cunit)
+        set_target_properties(test_rc4 PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${OUTPUT_DIRECTORY})
+        add_custom_command(TARGET test_rc4 
+            POST_BUILD 
+            COMMAND strip $<TARGET_FILE:test_rc4>
+            WORKING_DIRECTORY ${OUTPUT_DIRECTORY} 
+            COMMENT "Striping target") 
     endif()
 endfunction()
